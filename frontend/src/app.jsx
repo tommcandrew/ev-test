a831c16a69a5b3aec938027280daf4fa26644e19
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-13 00:08:59 +0100

    display formatted client createdAt date in table column

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { CLIENTS } from "./queries.js";
-import './index.css'
+import "./index.css";
 
 const App = () => {
   const { loading, error, data } = useQuery(CLIENTS, {
@@ -14,26 +14,35 @@ const App = () => {
   return (
     <div>
       <header>
-      <div className="clients__title">Client List</div>
+        <div className="clients__title">Client List</div>
       </header>
-        <div className="clients__container">
+      <div className="clients__container">
         <table className="clients__table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Company</th>
-          </tr>
-        </thead>
-        <tbody>
-        {data.clients.map((client) => <tr key={client.email} className="clients__row">
-          <td>{client.name}</td>
-          <td>{client.email}</td>
-          <td>{client.company}</td>
-        </tr>)}
-        </tbody>
-      </table>
-        </div>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => {
+              const formattedCreatedDate = new Date(
+                Number(client.createdAt)
+              ).toLocaleDateString("en-UK");
+              return (
+                <tr key={client.email} className="clients__row">
+                  <td>{client.name}</td>
+                  <td>{client.email}</td>
+                  <td>{client.company}</td>
+                  <td>{formattedCreatedDate}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
