8861f0bb2cd797a419e8814d536b72c9cdb91792
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-12 21:27:15 +0100

    set up Apollo server and connect to local DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,18 @@
-const express = require("express");
+import express from 'express';
+import mongoose from 'mongoose';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './typeDefs.js'
+import resolvers from './resolvers.js'
 
-const app = express();
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  const app = express();
+  server.applyMiddleware({ app });
+  mongoose.connect('mongodb://localhost:27017/client-list');
+  app.listen(3001, () => console.log('Server started on port 3001'));
+};
 
-app.listen(3001, () => {
-  console.log("Server started on port 3001");
-});
+startServer();
