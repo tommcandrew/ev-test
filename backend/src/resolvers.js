82ce57b377ca6dd957751415757a21ff89fb1818
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-13 20:10:12 +0100

    throw error in createClient resolver if client with email already exists

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -6,13 +6,15 @@ const resolvers = {
   },
   Mutation: {
     createClient: async (_, { name, email, company }) => {
+      const existingClient = await Client.findOne({ email });
+      if (existingClient) {
+        throw new Error("A client with this email already exists.")
+      }
       const client = new Client({ name, email, company });
       await client.save();
       return client;
     },
     deleteClient: async (_, { id }) => {
-      console.log("DELETE CLIENT RESOLVER")
-      console.log('id', id)
       await Client.deleteOne({ _id: id });
       return true;
     },
