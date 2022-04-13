6ab5fb9589cee4c1020d67d028787ec1682d49d8
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-13 20:33:27 +0100

    remove exclamation marks from all types for now

--- backend/src/typeDefs.js
+++ backend/src/typeDefs.js
@@ -2,18 +2,18 @@ import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
   type Query {
-    clients: [Client]!
+    clients: [Client]
   }
   type Mutation {
-    createClient(name: String, email: String, company: String): Client!
+    createClient(name: String, email: String, company: String): Client
     deleteClient(id: ID): Boolean
   }
   type Client {
-    id: ID!
-    name: String!
-    email: String!
-    company: String!
-    createdAt: String!
+    id: ID
+    name: String
+    email: String
+    company: String
+    createdAt: String
   }
 `;
 
