970f877a48580c531ce44a3513321444ea173373
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-14 14:53:08 +0100

    upgrade versions of react and react-testing-library and install react-test-rendered to fix mock apollo query issue

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 const client = new ApolloClient({
   uri: "http://localhost:3001/graphql",
@@ -14,4 +17,4 @@ const jsx = (
   </ApolloProvider>
 );
 
-ReactDOM.render(jsx, document.getElementById("root"));
+root.render(jsx);
