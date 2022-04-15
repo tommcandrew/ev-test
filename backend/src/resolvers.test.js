f28120fbb4bf6b64bc206e39c5b67211b993b8da
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-15 23:49:30 +0100

    install sinon and use for stubs in backend tests

--- backend/src/resolvers.test.js
+++ backend/src/resolvers.test.js
@@ -1,28 +1,41 @@
 import mongoose from "mongoose";
+import sinon from "sinon";
 import Client from "./models/Client";
 import resolvers from "./resolvers";
 
 describe("resolvers", () => {
+  let sandbox;
+  let mockSave;
+  let mockFind;
+  let mockFindOne;
+  let mockClients;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    mockSave = sandbox.stub(Client.prototype, "save");
+    mockFind = sandbox.stub(Client, "find");
+    mockFindOne = sandbox.stub(Client, "findOne");
+    mockClients = [
+      {
+        _id: mongoose.Types.ObjectId(),
+        name: "Bob",
+        email: "bob@gmail.com",
+        company: "Google",
+      },
+      {
+        _id: mongoose.Types.ObjectId(),
+        name: "Sally",
+        email: "sally@gmail.com",
+        company: "Microsoft",
+      },
+    ];
+    mockFindOne.resolves(null);
+    mockFind.resolves(mockClients);
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
   describe("queries", () => {
     describe("clients", () => {
-      let mockClients;
-      beforeAll(() => {
-        mockClients = [
-          {
-            _id: mongoose.Types.ObjectId(),
-            name: "Bob",
-            email: "bob@gmail.com",
-            company: "Google",
-          },
-          {
-            _id: mongoose.Types.ObjectId(),
-            name: "Sally",
-            email: "sally@gmail.com",
-            company: "Microsoft",
-          },
-        ];
-        Client.find = jest.fn().mockResolvedValue(mockClients);
-      });
       it("should fetch all clients", async () => {
         const res = await resolvers.Query.clients();
         expect(res).toEqual(mockClients);
@@ -32,19 +45,16 @@ describe("resolvers", () => {
   describe("mutations", () => {
     describe("createClient", () => {
       let clientData;
-      let mockSave;
       beforeAll(() => {
         clientData = {
           name: "Joe",
           email: "joe@gmail.com",
           company: "Apple",
         };
-        mockSave = jest.fn();
-        Client.prototype.save = mockSave;
       });
       it("should create new client", async () => {
         await resolvers.Mutation.createClient(null, clientData);
-        expect(mockSave).toHaveBeenCalledTimes(1);
+        sinon.assert.called(mockSave);
       });
     });
   });
