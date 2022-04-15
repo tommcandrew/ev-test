5a77cc6a45c2fec167fc1c74aae8e3500c03fb59
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-15 23:06:20 +0100

    update modal styles so mobile responsive

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -1,30 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ReactModal from "react-modal";
-import styles from './index.module.scss'
+import styles from "./index.module.scss";
 
 const Modal = ({ isOpen, closeModal, title, children }) => {
   if (process.env.NODE_ENV !== "test") ReactModal.setAppElement("#root");
 
-  const customStyles = {
-    overlay: {
-      position: "absolute",
-      backgroundColor: "#00507699",
-      zIndex: 20,
-    },
-    content: {
-      top: "10%",
-      left: "35%",
-      right: "auto",
-      bottom: "auto",
-      width: "30%",
-    },
-  };
   return (
     <ReactModal
       isOpen={isOpen}
-      style={customStyles}
       onRequestClose={closeModal}
+      className={styles.content}
+      overlayClassName={styles.overlay}
     >
       <div className={styles.layout}>
         <div className={styles.header}>
