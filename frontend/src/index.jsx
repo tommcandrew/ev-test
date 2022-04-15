ad1bfb80bbcaadf6435ec2b59dc01d740ba0dc75
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-15 17:07:31 +0100

    import and use normalize.css

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./components/App";
+import 'normalize.css/normalize.css';
 import "./styles/styles.scss";
 
 const container = document.getElementById("root");
