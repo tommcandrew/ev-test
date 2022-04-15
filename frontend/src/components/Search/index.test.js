016dd0e1b5cc9d4f853fe69652497bf8fcaf420d
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-15 21:59:21 +0100

    add tests for Search component

---
+++ frontend/src/components/Search/index.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, act, fireEvent } from "@testing-library/react";
+import Search from ".";
+
+describe("Search", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      onChange: jest.fn(),
+    };
+  });
+  function createComponent(p = props) {
+    return <Search {...props} />;
+  }
+  it("should render without error", () => {
+    render(createComponent());
+  });
+  it("should render search input", async () => {
+    const { getByLabelText } = render(createComponent());
+    const searchInput = getByLabelText("Search:");
+    expect(searchInput).toBeDefined();
+  });
+  it("should call onChange prop when input value changes", () => {
+    const { getByLabelText } = render(createComponent());
+    const searchInput = getByLabelText("Search:");
+    act(() => {
+      fireEvent.change(searchInput, { target: { value: "a" } });
+    });
+    expect(props.onChange).toHaveBeenCalled();
+  });
+});
